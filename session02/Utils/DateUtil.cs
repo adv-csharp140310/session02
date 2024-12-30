41c9c0888168691f1f763d94401cd5821e980598
Pashaie <pashaie@gmail.com>
2024-12-30 19:16:22 +0330

    GetJalaliMonthName

--- session02/Utils/DateUtil.cs
+++ session02/Utils/DateUtil.cs
@@ -4,6 +4,13 @@ using System.Text;
 namespace session02.Utils;
 public static class DateUtil
 {
+    static List<string> monthNames = new List<string>() {
+                "فروردین", "اردیبهشت", "خرداد",
+                "تیر", "مرداد",  "شهریور",
+                "مهر", "آبان", "آذر",
+                "دی", "بهمن", "اسفند"
+            };
+
     public static string MiladiToJalali(DateTime date)
     {
         //DRY - Don't Repeat Yourself 
@@ -33,5 +40,64 @@ public static class DateUtil
 
 
         return result; 
+    }
+
+    public static string GetJalaliMonthName(DateTime date)
+    {
+        //if/switch/array
+        var pc = new PersianCalendar();
+        var month = pc.GetMonth(date);
+        var result = "";
+
+        //switch (month)
+        //{
+        //    case 1: return "فروردین";
+        //    case 2: return "اردیبهشت";
+        //    case 3:
+        //        return "خرداد";
+        //    case 4:
+        //        return "تیر";
+        //    case 5:
+        //        return "مرداد";
+        //    case 6:
+        //        return "شهریور";
+        //    case 7:
+        //        return "مهر";
+        //    case 8:
+        //        return "آبان";
+        //    case 9:
+        //        return "آذر";
+        //    case 10:
+        //        return "دی";
+        //    case 11:
+        //        return "بهمن";
+        //    case 12:
+        //        return "اسفند";
+        //    default: throw new Exception("Wrong DATE!!");
+        //}
+
+        /***
+         * Statment
+         * Expression
+         */
+
+        //return month switch
+        //{
+        //    1 => "فروردین",
+        //    2 => "اردیبهشت",
+        //    3 => "خرداد",
+        //    4 => "تیر",
+        //    5 => "مرداد",
+        //    6 => "شهریور",
+        //    7 => "مهر",
+        //    8 => "آبان",
+        //    9 => "آذر",
+        //    10 => "دی",
+        //    11 => "بهمن",
+        //    12 => "اسفند",
+        //    _ => throw new Exception("Wrong DATE!!"),
+        //};
+
+        return monthNames[month - 1];
     }
 }
